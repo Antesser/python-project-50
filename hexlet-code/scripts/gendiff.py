cf993c21a6d5fd991992f9fc779a79fd6ff4fb65
Antesser <ahriman.kun@gmail.com>
2022-08-23 11:30:58 +0300

    First commit

---
+++ hexlet-code/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def main():
+    print('gendiff -h'
+            'usage: gendiff [-h] first_file second_file'
+
+            'Compares two configuration files and shows a difference.'
+
+            'positional arguments:'
+            'first_file'
+            'second_file'
+
+            'optional arguments:'
+            '-h, --help            show this help message and exit')
+
+
+if __name__ == '__main__':
+    main()
