430d4fa35b8b6e3c1fd07614434b164e3155d8f6
Antesser <ahriman.kun@gmail.com>
2022-09-27 13:42:33 +0300

    Changed prettify again

--- gendiff/formatters.py
+++ gendiff/formatters.py
@@ -1,9 +1,9 @@
 import json
 
 
-def prettify(dictionary):
+def prettify(dic):
     diff = '{\n'
-    dicstr = prettify_d(dictionary)
+    dicstr = prettify_dic(dic)
     indent = ' ' * 2
     dicstr = indent + dicstr.replace('\n', '\n' + indent)
     diff += dicstr
@@ -11,26 +11,26 @@ def prettify(dictionary):
     return diff
 
 
-def prettify_d(dictionary):
-    statuses = ('changed', 'added', 'deleted', 'unchanged', 'changeddict')
+def prettify_dic(dic):
+    variable = ('changed', 'added', 'deleted', 'unchanged', 'changeddict')
     diff = ''
-    for i in dictionary.items():
+    for i in dic.items():
         if isinstance(i[1][1], dict):
             value = prettify(i[1][1])
         else:
             value = i[1][1]
-        if i[1][0] not in statuses:
-            diff += f'{i[0]}: {i[1]}\n'
+        if i[1][0] not in variable:
+            diff += f'  {i[0]}: {i[1]}\n'
             return diff + '}'
         if i[1][0] == 'changed':
-            diff += f'- {i[0]}: {i[1][1]}\n'
-            diff += f'+ {i[0]}: {i[1][2]}\n'
+            diff += f"- {i[0]}: {i[1][1]}\n"
+            diff += f"+ {i[0]}: {i[1][2]}\n"
         if i[1][0] == 'added':
-            diff += f'+ {i[0]}: {value}\n'
+            diff += f"+ {i[0]}: {value}\n"
         if i[1][0] == 'deleted':
-            diff += f'- {i[0]}: {value}\n'
+            diff += f"- {i[0]}: {value}\n"
         if i[1][0] == 'unchanged' or i[1][0] == 'changeddict':
-            diff += f'  {i[0]}: {value}\n'
+            diff += f"  {i[0]}: {value}\n"
     return diff + '}'
 
 
