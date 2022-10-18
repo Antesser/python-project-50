0026aca573b9320e2a1ada8cc0a09b1459255f8e
Alex <Alex@Antesser.local>
2022-10-18 20:14:25 +0300

    Changed prettify

--- gendiff/formatters.py
+++ gendiff/formatters.py
@@ -1,33 +1,35 @@
 SPACES = '  '
 
 
-def prettify(lst, lvl=0):
+def prettify_val(value, lvl):
+    return [value['status'], value['key'], value['value']]
+
+
+def prettify(lst, lvl=1):
     diff = ''
     for key in lst:
         if isinstance(key['value'], list):
-            value = prettify(key['value'], lvl+1)
+            value = prettify(key['value'], lvl)
         try:
             if isinstance(key['value'], dict):
-                value = prettify(key['value'], lvl+1)
+                value = prettify(key['value'], lvl)
         except TypeError:
-            value = key['value']
+            value = prettify_val(key, lvl)
         else:
-            value = key['value']
-        diff += SPACES * lvl
-        if key['status'] == 'changed':
-            diff += f"- {key['key']}: {key['value']['old_value']}\n"
-            diff += SPACES * lvl
-            diff += f"+ {key['key']}: {key['value']['new_value']}\n"
-        if key['status'] == 'added':
-            diff += f"+ {key['key']}: {value}\n"
-        if key['status'] == 'deleted':
-            diff += f"- {key['key']}: {value}\n"
-        if key['status'] == 'unchanged':
-            diff += f"  {key['key']}: {value}\n"
-        if key['status'] == 'changeddict':
-            diff += f"{key['key']}: {'{'}{value}\n"
-    diff = diff.replace('{{', '{')
-    return '{\n' + diff + '}'
+            value = prettify_val(key, lvl)
+        indent = SPACES * lvl
+        if value[0] == 'changed':
+            diff += f"{indent}- {value[1]}: {value[2]['old_value']}\n"
+            diff += f"{indent}+ {value[1]}: {value[2]['new_value']}\n"
+        elif value[0] == 'added':
+            diff += f"{indent}+ {value[1]}: {value[2]}\n"
+        elif value[0] == 'deleted':
+            diff += f"{indent}- {value[1]}: {value[2]}\n"
+        elif value[0] == 'unchanged':
+            diff += f"{indent}  {value[1]}: {value[2]}\n"
+        elif value[0] == 'changeddict':
+            diff += f"  {indent}{value[1]}:{prettify(value[2], lvl+1)}\n"
+    return '{\n' + diff + indent + '}'
 
 
 def create_plain(lst, addon='', line=''):
