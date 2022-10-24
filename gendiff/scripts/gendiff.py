4053e987382f9521760dc2f651e93050ad7ee3e5
Antesser <ahriman.kun@gmail.com>
2022-10-24 13:48:33 +0300

    Starter has been moved into script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ from gendiff import cli, engine
 
 def main():
     args = cli.parse_args()
-    engine.start_program(args)
+    print(engine.generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
