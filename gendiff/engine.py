1263ef5dd6222405a9933be9d36b50d6e797cf86
Antesser <ahriman.kun@gmail.com>
2022-10-25 21:01:19 +0300

    Third iteration of fixes

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,13 +1,13 @@
 from gendiff.formatters.create_json import create_json
 from gendiff.formatters.create_plain import create_plain
 from gendiff.formatters.prettify import prettify
-from gendiff.parser import parser
+from gendiff.parser import check_extension, file_loader, parser
 from gendiff.create_diff import create_diff
 
 
 def generate_diff(first_file, second_file, format=''):
-    first = parser(first_file)
-    second = parser(second_file)
+    first = parser(file_loader(first_file), check_extension(first_file))
+    second = parser(file_loader(second_file), check_extension(second_file))
     diff = create_diff(first, second)
     if format == 'plain':
         diff = create_plain(diff)
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,18 +3,17 @@ import os
 import yaml
 
 
+def check_extension(path):
+    return os.path.splitext(path)[-1]
+
+
 def file_loader(path):
-    file_extension = os.path.splitext(path)[-1]
-    if file_extension == '.json':
-        data = json.load(open(path))
-    elif file_extension == '.yml' or '.yaml':
-        with open(path, 'r') as file:
-            data = yaml.safe_load(file)
-    else:
-        raise ValueError('Inputed format is not supported')
-    return data
+    with open(path) as file:
+        return file.read()
 
 
-def parser(path):
-    data = file_loader(path)
-    return data
+def parser(path, extension):
+    if extension == 'json':
+        return json.loads(path)
+    elif extension == 'yaml' or 'yml':
+        return yaml.safe_load(path)
