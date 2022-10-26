06ce8d750d12d6e17a1becd77f7de063781d6b5b
Antesser <ahriman.kun@gmail.com>
2022-10-26 20:16:01 +0300

    Added default value into parse_args

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -5,7 +5,7 @@ from gendiff.parser import find_extension, load_file, parser
 from gendiff.create_diff import create_diff
 
 
-def generate_diff(first_file, second_file, format):
+def generate_diff(first_file, second_file, format='stylish'):
     first = parser(load_file(first_file), find_extension(first_file))
     second = parser(load_file(second_file), find_extension(second_file))
     diff = create_diff(first, second)
