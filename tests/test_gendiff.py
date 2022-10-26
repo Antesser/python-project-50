928b3e65f3622d7772e17f2021c1b08906dcfdbe
Antesser <ahriman.kun@gmail.com>
2022-10-26 19:04:24 +0300

    Added lots of tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,27 +4,51 @@ import pytest
 
 @pytest.mark.parametrize("first_file, second_file, result, format", [
     (
+        './tests/fixtures/first.yml',
+        './tests/fixtures/second.yml',
+        './tests/fixtures/answer_yaml.txt',
+        'stylish'
+    ),
+    (
         './tests/fixtures/first.json',
         './tests/fixtures/second.json',
-        './tests/fixtures/answer_j.y.txt',
+        './tests/fixtures/answer_yaml.txt',
         'stylish'
     ),
     (
         './tests/fixtures/first.yaml',
         './tests/fixtures/second.yaml',
-        './tests/fixtures/answer_j.y.txt',
+        './tests/fixtures/answer_nested.txt',
         'stylish'
     ),
     (
-        './tests/fixtures/first_nested.json',
-        './tests/fixtures/second_nested.json',
+        './tests/fixtures/first.json',
+        './tests/fixtures/second.json',
         './tests/fixtures/answer_nested.txt',
         'stylish'
+    ),
+    (
+        './tests/fixtures/first_nested.yaml',
+        './tests/fixtures/second_nested.yaml',
+        './tests/fixtures/answer_plain.txt',
+        'plain'
     ),
     (
         './tests/fixtures/first_nested.json',
         './tests/fixtures/second_nested.json',
         './tests/fixtures/answer_plain.txt',
+        'plain'
+    ),
+    (
+        './tests/fixtures/first_nested.yaml',
+        './tests/fixtures/second_nested.yaml',
+        './tests/fixtures/answer_nested_json.txt',
+        'plain'
+    ),
+    (
+        './tests/fixtures/first_nested.json',
+        './tests/fixtures/second_nested.json',
+        './tests/fixtures/answer_nested_json.txt',
         'plain'
     ),
 ])
