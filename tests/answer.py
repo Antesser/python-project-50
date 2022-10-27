2b864e919a9a2be4b896d5f7ecd9dc2c39d4a727
Antesser <ahriman.kun@gmail.com>
2022-10-27 09:23:49 +0300

    Added test coverage

---
+++ tests/answer.py
@@ -0,0 +1,67 @@
+YAML = '''{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
+
+NESTED_JSON = '''[{"key": "common", "status": "nested", "value": [{"key": "follow", "status": "added", "value": false}, {"key": "setting1", "status": "unchanged", "value": "Value 1"}, {"key": "setting2", "status": "deleted", "value": 200}, {"key": "setting3", "status": "changed", "value": {"old_value": true, "new_value": null}}, {"key": "setting4", "status": "added", "value": "blah blah"}, {"key": "setting5", "status": "added", "value": {"key5": "value5"}}, {"key": "setting6", "status": "nested", "value": [{"key": "doge", "status": "nested", "value": [{"key": "wow", "status": "changed", "value": {"old_value": "", "new_value": "so much"}}]}, {"key": "key", "status": "unchanged", "value": "value"}, {"key": "ops", "status": "added", "value": "vops"}]}]}, {"key": "group1", "status": "nested", "value": [{"key": "baz", "status": "changed", "value": {"old_value": "bas", "new_value": "bars"}}, {"key": "foo", "status": "unchanged", "value": "bar"}, {"key": "nest", "status": "changed", "value": {"old_value": {"key": "value"}, "new_value": "str"}}]}, {"key": "group2", "status": "deleted", "value": {"abc": 12345, "deep": {"id": 45}}}, {"key": "group3", "status": "added", "value": {"deep": {"id": {"number": 45}}, "fee": 100500}}]'''
+
+PLAIN = '''Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]'''
+
+NESTED = '''{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}'''
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,55 +1,56 @@
 from gendiff.engine import generate_diff
 import pytest
+from tests import answer
 
 
 @pytest.mark.parametrize("first_file, second_file, result, format", [
     (
-        './tests/fixtures/first.yml',
-        './tests/fixtures/second.yml',
-        './tests/fixtures/answer_yaml.txt',
-        'stylish'
-    ),
-    (
-        './tests/fixtures/first.json',
-        './tests/fixtures/second.json',
-        './tests/fixtures/answer_yaml.txt',
-        'stylish'
-    ),
-    (
         './tests/fixtures/first.yaml',
         './tests/fixtures/second.yaml',
-        './tests/fixtures/answer_nested.txt',
+        answer.YAML,
         'stylish'
     ),
     (
         './tests/fixtures/first.json',
         './tests/fixtures/second.json',
-        './tests/fixtures/answer_nested.txt',
+        answer.YAML,
         'stylish'
     ),
     (
         './tests/fixtures/first_nested.yaml',
         './tests/fixtures/second_nested.yaml',
-        './tests/fixtures/answer_plain.txt',
+        answer.NESTED,
+        'stylish'
+    ),
+    (
+        './tests/fixtures/first_nested.json',
+        './tests/fixtures/second_nested.json',
+        answer.NESTED,
+        'stylish'
+    ),
+    (
+        './tests/fixtures/first_nested.yaml',
+        './tests/fixtures/second_nested.yaml',
+        answer.PLAIN,
         'plain'
     ),
     (
         './tests/fixtures/first_nested.json',
         './tests/fixtures/second_nested.json',
-        './tests/fixtures/answer_plain.txt',
+        answer.PLAIN,
         'plain'
     ),
     (
         './tests/fixtures/first_nested.yaml',
         './tests/fixtures/second_nested.yaml',
-        './tests/fixtures/answer_nested_json.txt',
-        'plain'
+        answer.NESTED_JSON,
+        'json'
     ),
     (
         './tests/fixtures/first_nested.json',
         './tests/fixtures/second_nested.json',
-        './tests/fixtures/answer_nested_json.txt',
-        'plain'
+        answer.NESTED_JSON,
+        'json'
     ),
 ])
 def test_gendiff(first_file, second_file, result, format):
